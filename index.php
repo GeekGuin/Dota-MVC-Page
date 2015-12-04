936f857075664e610f5425e9a2eb3dcb05ae4b6d
Victor Tortolero <victormtortolero@gmail.com>
2015-12-04 01:14:22 -0430

    Merge from backEnd and backEnd1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	require_once('connection.php');
+
+	if(isset($_GET['controller']) && isset($_GET['action'])){
+		$controller = $_GET['controller'];
+		$action = $_GET['action'];
+	}else{
+		$controller = 'main';
+		$action = 'index';
+	}
+
+	if(isset($_GET['lang'])){
+		$lang = $_GET['lang'];
+	}else{
+		$lang = 'es';
+	}
+
+?>
